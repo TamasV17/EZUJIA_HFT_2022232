e1e1592fcced089839e2ad17b32faf0f454fa1df
Tamás Varga <varga.tamas117@gmail.com>
2023-05-18 20:15:10 +0200

    Class Cars have been created

--- EZUJIA_HFT_2022232.Models/Cars.cs
+++ EZUJIA_HFT_2022232.Models/Cars.cs
@@ -7,7 +7,42 @@ namespace EZUJIA_HFT_2022232.Models
 {
     public class Cars
     {
+        public string Brand { get; set; }
 
-       
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RentcarId { get; set; }
+        public int CarsID { get; set; }
+
+        public string Type { get; set; }
+
+        public string LicensePlateNumber { get; set; }
+
+        public int Year { get; set; }
+
+        public int PerformanceInHP { get; set; }
+
+        //public int EmployeesId { get; set; }
+
+        public virtual ICollection<Rent> Owner { get; set; }
+
+        public Cars(string path)
+        {
+            string[] splitarray = path.Split(",");
+            Brand = splitarray[0];
+            RentcarId = int.Parse(splitarray[1]);
+            Type = splitarray[2];
+            LicensePlateNumber = splitarray[3];
+            Year = int.Parse(splitarray[4]);
+            PerformanceInHP = int.Parse(splitarray[4]);
+            //EmployeesId = int.Parse(splitarray[5]);
+            //Owner = new HashSet<Employees>();
+
+        }
+        public Cars()
+        {
+            //Owner = new HashSet<Employees>();
+        }
+
     }
 }
