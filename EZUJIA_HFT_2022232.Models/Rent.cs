64662f5b0bc611751071ab0b4e79c27f93baccb0
Tamás Varga <varga.tamas117@gmail.com>
2023-05-18 20:16:35 +0200

    Class Rent have been created.

--- EZUJIA_HFT_2022232.Models/Rent.cs
+++ EZUJIA_HFT_2022232.Models/Rent.cs
@@ -10,6 +10,20 @@ namespace EZUJIA_HFT_2022232.Models
 {
     public class Rent
     {
-       
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RentId { get; set; }
+
+        public string RentTime { get; set; }
+
+
+        public int RentcarId { get; set; }
+
+        public int EmployeesID { get; set; }
+
+        public virtual Cars CarId { get; private set; }
+
+        public virtual Employees Employees { get; private set; }
+
     }
 }
