500774f22e6599e61d3d7b64edbd26a030d31b94
Tamás Varga <varga.tamas117@gmail.com>
2023-05-18 20:18:47 +0200

    Database Context have been created successfully.

--- EZUJIA_HFT_2022232.Repository/MyDbContext.cs
+++ EZUJIA_HFT_2022232.Repository/MyDbContext.cs
@@ -8,7 +8,59 @@ using System.Threading.Tasks;
 
 namespace EZUJIA_HFT_2022232.Repository
 {
-    public class MyDbContext
-    { 
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Cars> cars { get; set; }
+
+        public DbSet<Employees> employees { get; set; }
+
+        public DbSet<Rent> rents { get; set; }
+
+        public MyDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseInMemoryDatabase("data").UseLazyLoadingProxies();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            modelBuilder.Entity<Rent>().HasOne(t => t.CarId).WithMany(t => t.Owner).HasForeignKey(t => t.RentcarId);
+
+            modelBuilder.Entity<Rent>().HasOne(t => t.Employees).WithMany(t => t.RentCar).HasForeignKey(t => t.EmployeesID);
+
+
+
+
+
+            modelBuilder.Entity<Cars>().HasData(new Cars[]
+            {
+
+            });
+
+            modelBuilder.Entity<Employees>().HasData(new Employees[]
+            {
+
+            });
+
+            modelBuilder.Entity<Rent>().HasData(new Rent[]
+            {
+
+            });
+
+
+        }
+
+
+
+
     }
 }
