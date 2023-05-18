8be775abb25dd8da413bb86e45bb2f7fa2cd49b5
Tamás Varga <varga.tamas117@gmail.com>
2023-05-18 20:27:09 +0200

    EmployeesLogic have been created in layer Logic.

--- EZUJIA_HFT_2022232.Logic/EmployeesLogic.cs
+++ EZUJIA_HFT_2022232.Logic/EmployeesLogic.cs
@@ -8,7 +8,47 @@ using System.Threading.Tasks;
 
 namespace EZUJIA_HFT_2022232.Logic
 {
-    class EmployeesLogic
+    class EmployeesLogic : IEmployeesLogic
     {
+        IRepository<Employees> repo;
+        public void Create(Employees item)
+        {
+            if (item.Name.Length < 5)
+            {
+                throw new Exception("The name is too short");
+            }
+            else
+            {
+                repo.Create(item);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Employees Read(int id)
+        {
+            var item = this.repo.Read(id);
+            if (item == null)
+            {
+                throw new Exception("The employee you entered not exists");
+            }
+            else
+            {
+                return item;
+            }
+        }
+
+        public IEnumerable<Employees> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Employees item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
