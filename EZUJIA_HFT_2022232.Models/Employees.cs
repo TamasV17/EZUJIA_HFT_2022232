4616740d602573635160db0eae1b7c568375b9a5
Tamás Varga <varga.tamas117@gmail.com>
2023-05-18 20:15:56 +0200

    Class Employees have been created.

--- EZUJIA_HFT_2022232.Models/Employees.cs
+++ EZUJIA_HFT_2022232.Models/Employees.cs
@@ -10,8 +10,38 @@ namespace EZUJIA_HFT_2022232.Models
 {
     public class Employees
     {
+        public string Name { get; set; }
 
-      
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmployeesID { get; set; }
+
+        public string ClassName { get; set; }
+
+        public string PhoneNumer { get; set; }
+
+        public string DateOfBirth { get; set; }
+        public virtual ICollection<Rent> RentCar { get; set; }
+
+
+        public Employees()
+        {
+            //RentCar = new HashSet<Cars>();
+        }
+
+        public Employees(string path)
+        {
+            string[] splitarray = path.Split(",");
+            Name = splitarray[0];
+            EmployeesID = int.Parse(splitarray[1]);
+            ClassName = splitarray[2];
+            PhoneNumer = splitarray[3];
+            DateOfBirth = splitarray[4];
+            RentCar = new HashSet<Rent>();
+
+
+
         }
     }
+}
 
