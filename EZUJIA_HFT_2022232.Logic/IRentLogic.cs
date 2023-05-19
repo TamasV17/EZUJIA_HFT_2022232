562df052ea52be34445ab08605de4a9e859caa87
Tamás Varga <varga.tamas117@gmail.com>
2023-05-19 21:59:55 +0200

    Interface IRentLogic has been created.

---
+++ EZUJIA_HFT_2022232.Logic/IRentLogic.cs
@@ -0,0 +1,22 @@
+﻿using EZUJIA_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZUJIA_HFT_2022232.Logic
+{
+    internal interface IRentLogic
+    {
+        void Create(Rent item);
+
+        void Update(Rent item);
+
+        void Delete(int id);
+
+        Rent Read(int id);
+
+        IEnumerable<Rent> ReadAll();
+    }
+}
