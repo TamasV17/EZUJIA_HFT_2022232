5cdc2a22df807223e796161b66d68b1538e68fce
Tamás Varga <varga.tamas117@gmail.com>
2023-05-19 22:01:42 +0200

    Class RentLogic has been created.

---
+++ EZUJIA_HFT_2022232.Logic/RentLogic.cs
@@ -0,0 +1,47 @@
+﻿using EZUJIA_HFT_2022232.Models;
+using EZUJIA_HFT_2022232.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZUJIA_HFT_2022232.Logic
+{
+    class RentsLogic : IRentLogic
+    {
+        IRepository<Rent> repo;
+        public void Create(Rent item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Rent Read(int id)
+        {
+            var item = this.repo.Read(id);
+            if (item == null)
+            {
+                throw new Exception("The rent id not exists");
+            }
+            else
+            {
+                return item;
+            }
+        }
+
+        public IEnumerable<Rent> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Rent item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
